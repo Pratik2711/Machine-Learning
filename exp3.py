e03496c519246dcabfa5362e8572b68658432705
Pratik Amrutsagar <pratikamrutsagar2711@gmail.com>
2021-03-18 19:23:08 +0530

    unnecessary files deleted

--- exp3.py
+++
@@ -1,5 +0,0 @@
-'''2. Suppose you have given the stock prices for respective days like (100, 695, 260, 310, 40, 535, 695). 
-The stock price for the 1st day is 100, 2nd day it is 180 and so on. Find the maximum profit that
- can be earned and the days buying the stock and selling the stock. Note that buying day is earlier 
- than the selling day. 
- For the given example the maximum profit is 655 and the days are buying day: Day 5 and selling day: Day 7.'''
